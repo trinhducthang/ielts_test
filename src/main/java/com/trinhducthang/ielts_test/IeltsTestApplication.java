ac9e95fbe44bc69cd706e23e865dc38d29570c59
thang7904 <trinhducthang.7904@gmail.com>
2024-06-28 23:07:08 +0700

    fist update

---
+++ src/main/java/com/trinhducthang/ielts_test/IeltsTestApplication.java
@@ -0,0 +1,13 @@
+package com.trinhducthang.ielts_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IeltsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IeltsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trinhducthang/ielts_test/IeltsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trinhducthang.ielts_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IeltsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
